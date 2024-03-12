3ca92953e013173b06d504afe76397bc712ee3af
Defancet <xkalut00@stud.fit.vutbr.cz>
2024-03-12 21:34:21 +0100

    feat: added functions for evaluating expressions

--- src/calc.py
+++ src/calc.py
@@ -14,4 +14,41 @@ def divide(x, y):
     if y != 0:
         return x / y
     else:
-        return "Error! Division by zero."
+        return None
+
+
+def infix_to_postfix(expression):
+    precedence = {'+':1, '-':1, '*':2, '/':2}
+    stack = []
+    postfix = []
+    for char in expression:
+        if char.isdigit():
+            postfix.append(char)
+        elif char in "+-*/":
+            while stack and precedence[stack[-1]] >= precedence[char]:
+                postfix.append(stack.pop())
+            stack.append(char)
+    while stack:
+        postfix.append(stack.pop())
+    return postfix
+
+
+def evaluate_postfix(postfix):
+    stack = []
+    for char in postfix:
+        if char.isdigit():
+            stack.append(int(char))
+        else:
+            b, a = stack.pop(), stack.pop()
+            if char == '+': result = a + b
+            elif char == '-': result = a - b
+            elif char == '*': result = a * b
+            elif char == '/': result = a / b
+            stack.append(result)
+    return stack[0]
+
+
+def evaluate_expression(expression):
+    postfix = infix_to_postfix(expression.replace(" ", ""))
+    return evaluate_postfix(postfix)
+
